dd224bfa5715fe8dd27d51f2fcb10a0e72d3c3e3
aericpp <p.yue89@gmail.com>
2023-08-09 13:03:36 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,12 @@ export GITHUB="true"
 echo "INPUT_DEPEND_LIST $INPUT_DEPEND_LIST"
 echo "INPUT_SCRIPT_FILE $INPUT_SCRIPT_FILE"
 
-echo "* $*"
-echo "@ $@"
-echo "# $#"
-echo "？ $?"
-echo "depend_list $depend_list"
-echo "script_file $script_file"
+echo $*
+echo $@
+echo $#
+echo $?
+echo $depend_list
+echo $script_file
 
 apt update
 apt install -qy "$depend_list"
